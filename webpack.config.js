9160dd1f1266f2a4750236d0ae79ae1885991183
906bc <57206558+906bc906@users.noreply.github.com>
2022-09-17 13:29:54 +0900

    html-webpack-plugin 설치

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './sources/index.js',
   output: {
     path: path.resolve(__dirname, "./public"),
     filename: 'index.js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './sources/index.html'
+    })
+  ]
 };
